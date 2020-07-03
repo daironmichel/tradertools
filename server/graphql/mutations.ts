5f9f38bb0216f093caabab81f6055d4522cde927
Dairon M. Leyva <daironmichel@gmail.com>
2020-07-03 14:36:42 -0400

    attempt to fix typing for mutations. WIP: schema broken

--- server/graphql/mutations.ts
+++ server/graphql/mutations.ts
@@ -3,15 +3,41 @@ import { MaybeUserRecord } from '../db';
 import db, { User } from '../db/db';
 import { compare } from '../utils';
 import jwt from 'jsonwebtoken';
-import { GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLInputObjectType } from 'graphql';
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLInputObjectType,
+  GraphQLFieldConfig,
+  GraphQLFieldConfigArgumentMap,
+} from 'graphql';
 import { UserNode } from './types';
 
-interface ILoginPayload {
+type ILoginArgs = {
+  input: {
+    username: string;
+    password: string;
+  };
+};
+
+type ILoginPayload = {
   user?: MaybeUserRecord;
   accessToken?: string;
   refreshToken?: string;
   error?: string;
-}
+};
+
+export const LoginArgs: GraphQLFieldConfigArgumentMap = {
+  input: {
+    type: new GraphQLInputObjectType({
+      name: 'LoginInput',
+      fields: {
+        username: { type: new GraphQLNonNull(GraphQLString) },
+        password: { type: new GraphQLNonNull(GraphQLString) },
+      },
+    }),
+  },
+};
 
 export const LoginPayload = new GraphQLObjectType({
   name: 'LoginPayload',
@@ -23,66 +49,59 @@ export const LoginPayload = new GraphQLObjectType({
   },
 });
 
-export const LoginInput = new GraphQLInputObjectType({
-  name: 'LoginInput',
-  fields: {
-    username: { type: new GraphQLNonNull(GraphQLString) },
-    password: { type: new GraphQLNonNull(GraphQLString) },
+const LoginMutation: GraphQLFieldConfig<Object, Context, ILoginArgs> = {
+  args: LoginArgs,
+  type: LoginPayload,
+  resolve: async (_source, args, context, _info): Promise<ILoginPayload> => {
+    if (context.user) return { error: "You're already logged in" };
+    const { username, password } = args.input;
+    const user: MaybeUserRecord = await db<User>('User')
+      .where({ username: username })
+      .select(['id', 'username', 'firstName', 'lastName', 'password'])
+      .first();
+
+    if (!user || !user.password) return { error: 'Incorrect username or password' };
+    if (!compare(password, user.password)) return { error: 'Incorrect username or password' };
+
+    const secretKey = process.env.SECRET_KEY;
+    if (!secretKey) throw 'SECRET_KEY environment variables required.';
+
+    const accessToken = jwt.sign({ id: user.id }, secretKey, { expiresIn: '1m' });
+    delete user['password'];
+    return { user, accessToken };
   },
-});
+};
 
-export const PublicMutation = new GraphQLObjectType({
-  name: 'PublicMutation',
-  fields: {
-    login: {
-      type: LoginPayload,
-      args: {
-        input: { type: LoginInput },
-      },
-      resolve: async (_source, args, context, _info): Promise<ILoginPayload> => {
-        if (context.user) return { error: "You're already logged in" };
-        const { username, password } = args.input;
-        const user: MaybeUserRecord = await db<User>('User')
-          .where({ username: username })
-          .select(['id', 'username', 'firstName', 'lastName', 'password'])
-          .first();
-
-        if (!user || !user.password) return { error: 'Incorrect username or password' };
-        if (!compare(password, user.password)) return { error: 'Incorrect username or password' };
-
-        const secretKey = process.env.SECRET_KEY;
-        if (!secretKey) throw 'SECRET_KEY environment variables required.';
-
-        const accessToken = jwt.sign({ id: user.id }, secretKey, { expiresIn: '1m' });
-        delete user['password'];
-        return { user, accessToken };
-      },
-    },
+export const PublicMutation: GraphQLFieldConfig<Object, Context> = {
+  type: new GraphQLObjectType({
+    name: 'PublicMutation',
+    fields: (): any => ({
+      login: LoginMutation,
+    }),
+  }),
+  resolve() {
+    return {};
   },
-});
+};
 
-export const PrivateMutation = new GraphQLObjectType({
-  name: 'PrivateMutation',
-  fields: {
-    dosomething: { type: GraphQLString },
+export const PrivateMutation: GraphQLFieldConfig<Object, Context> = {
+  type: new GraphQLObjectType({
+    name: 'PrivateMutation',
+    fields: (): any => ({
+      todo: { type: GraphQLString },
+    }),
+  }),
+  resolve: (_source, _args, context: Context) => {
+    context.reply.status(401); // unauthorized;
+    throw 'Not Authorized';
   },
-});
+};
 
 export const MutationType = new GraphQLObjectType({
   name: 'Mutation',
   fields: () => ({
-    public: {
-      type: PublicMutation,
-      resolve() {
-        return {};
-      },
-    },
-    private: {
-      type: PrivateMutation,
-      resolve: (_source, _args, _context: Context) => {
-        throw 'Not Authorized';
-      },
-    },
+    public: PublicMutation,
+    private: PrivateMutation,
   }),
 });
 
